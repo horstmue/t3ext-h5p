1710ee6154f5a00e84bb7919661af9af2932af9f
horstmue <hm@horst-muerdter.com>
2026-04-01 12:28:08 -0600

    Remove legacy configurations and bump versions to 3.0.0
    Removed outdated extension configurations and updated several packages (`dws-security`, `photoswipe`) to version 3.0.0. Adjustments include TCA changes, compatibility improvements, and cleanup of deprecated setups for TYPO3 integrations.

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,19 +1,21 @@
 <?php
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') or die('¯\_(ツ)_/¯');
 
+/* --------------------------------------------------------------------------
+ * Add H5P content element types to the CType selector
+ * ------------------------------------------------------------------------ */
 ExtensionManagementUtility::addTcaSelectItem(
     'tt_content',
     'CType',
     [
-        'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:h5p.wizard.title',
-        'h5p_view',
-        'h5p-logo',
-        'H5P',
-        'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:h5p.wizard.description'
+        'label' => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:h5p.wizard.title',
+        'value' => 'h5p_view',
+        'icon'  => 'h5p-logo',
+        'group' => 'H5P',
+        'description' => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:h5p.wizard.description',
     ]
 );
 
@@ -21,78 +23,91 @@ ExtensionManagementUtility::addTcaSelectItem(
     'tt_content',
     'CType',
     [
-        'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:h5p.statistics',
-        'h5p_statistics',
-        'h5p-logo',
-        'H5P',
-        'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:h5p.statistics.description'
+        'label' => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:h5p.statistics',
+        'value' => 'h5p_statistics',
+        'icon'  => 'h5p-logo',
+        'group' => 'H5P',
+        'description' => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:h5p.statistics.description',
     ]
 );
 
+/* --------------------------------------------------------------------------
+ * Add new columns to tt_content
+ * ------------------------------------------------------------------------ */
 ExtensionManagementUtility::addTCAcolumns(
     'tt_content',
     [
-        'tx_h5p_content'         => [
-            'exclude' => 1,
+        'tx_h5p_content' => [
+            'exclude' => true,
             'label'   => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:tt_content.tx_h5p_content',
             'config'  => [
                 'type'          => 'group',
-                'rootLevel'     => true,
                 'allowed'       => 'tx_h5p_domain_model_content',
-                'items'         => [
-                    ['', 0]
-                ],
+                'foreign_table' => 'tx_h5p_domain_model_content',
                 'default'       => 0,
                 'size'          => 1,
                 'minitems'      => 0,
                 'maxitems'      => 1,
-                'wizards'       => [
-                    'suggest' => [
-                        'type' => 'suggest',
-                    ]
-                ]
-            ]
+                // Der 'suggest' Wizard ist seit TYPO3 12+ für 'group' Felder Standard
+                // und benötigt keine explizite 'wizards' Definition mehr.
+            ],
         ],
         'tx_h5p_display_options' => [
-            'exclude' => 1,
+            'exclude' => true,
             'label'   => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:tt_content.tx_h5p_display_options',
             'config'  => [
                 'type'    => 'check',
                 'items'   => [
-                    ['label' => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:tt_content.tx_h5p_display_options.I.0', ''], // 1
-                    ['label' => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:tt_content.tx_h5p_display_options.I.1', ''], // 2
-                    ['label' => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:tt_content.tx_h5p_display_options.I.2', ''], // 4
-                    ['label' => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:tt_content.tx_h5p_display_options.I.3', ''], // 8
-                    ['label' => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:tt_content.tx_h5p_display_options.I.4', ''], // 16
+                    [
+                        'label' => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:tt_content.tx_h5p_display_options.I.0',
+                        'value' => 1,
+                    ],
+                    [
+                        'label' => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:tt_content.tx_h5p_display_options.I.1',
+                        'value' => 2,
+                    ],
+                    [
+                        'label' => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:tt_content.tx_h5p_display_options.I.2',
+                        'value' => 4,
+                    ],
+                    [
+                        'label' => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:tt_content.tx_h5p_display_options.I.3',
+                        'value' => 8,
+                    ],
+                    [
+                        'label' => 'LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:tt_content.tx_h5p_display_options.I.4',
+                        'value' => 16,
+                    ],
                 ],
                 'cols'    => 2,
-                'default' => H5PCore::DISABLE_FRAME + H5PCore::DISABLE_COPYRIGHT // 1 + 8
+                // Falls H5PCore hier nicht verfügbar ist (Namespace), harten Wert nutzen oder Konstante korrekt importieren
+                'default' => 9,
             ],
         ],
     ]
 );
 
+/* --------------------------------------------------------------------------
+ * Palette and Type Configuration
+ * ------------------------------------------------------------------------ */
 ExtensionManagementUtility::addFieldsToPalette(
     'tt_content',
     'h5p',
-    'tx_h5p_content,--linebreak--,tx_h5p_display_options'
+    'tx_h5p_content, --linebreak--, tx_h5p_display_options'
 );
 
-// This is needed for addToAllTCAtypes to work
-$GLOBALS['TCA']['tt_content']['types']['h5p_view']['showitem'] = '';
-
-ExtensionManagementUtility::addToAllTCAtypes(
-    'tt_content',
-    '--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
-     --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.header;header,
-     --palette--;LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:tt_content.tx_h5p_display_options;h5p,
-     --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
-     --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
-     --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
-     --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.visibility;visibility,
-     --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
-     --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.extended
+$GLOBALS['TCA']['tt_content']['types']['h5p_view'] = [
+    'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;;general,
+            --palette--;;header,
+            --palette--;LLL:EXT:h5p/Resources/Private/Language/Tca.xlf:tt_content.tx_h5p_display_options;h5p,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+            --palette--;;frames,
+            --palette--;;appearanceLinks,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
     ',
-    'h5p_view',
-    'after:CType'
-);
+];
